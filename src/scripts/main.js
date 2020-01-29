2a3188aac364ba6fdbbd563d05d1e5e821f319c6
Jason Booth <hello@studiojasonbooth.com>
2020-01-29 11:29:03 +0000

    mouse inactive code added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,41 +1,69 @@
-const cubeTag = document.querySelector('div.cube')
+    
+    
+    const cubeTag = document.querySelector('div.cube')
 
-cubeTag.style.animation = 'spin 1.4s infinite linear'
+    cubeTag.style.animation = 'spin 2s infinite linear'
 
-document.addEventListener('mousemove', function (event) {
-  cubeTag.style.animation = ''
-})
+    document.addEventListener('mousemove', function (event) {
+      cubeTag.style.animation = ''
+    })
 
-document.addEventListener('mousemove', function (event) {
 
+    document.addEventListener('mousemove', function (event) {
+
+      const x = event.pageX
+      const y = event.pageY
+
+      const midX = x - window.innerWidth / 2
+      const midY = y - window.innerHeight / 2
+
+      cubeTag.style.left = x + 'px'
+      cubeTag.style.top = y + 'px'
+
+      cubeTag.style.transform = "rotateX(" + midY + "deg) rotateY(" + midX + "deg)"
+    })
+      
+
+
+    document.addEventListener("touchmove", function (event) {
+      const cubeTag = document.querySelector('div.cube')
+      
+      const x = event.pageX
+      const y = event.pageY
+      
+      const midX = x - window.innerWidth / 2
+      const midY = y - window.innerHeight / 2
+      
+      cubeTag.style.left = x + 'px'
+      cubeTag.style.top = y + 'px'
+
+      cubeTag.style.transform = "rotateX(" + midY + "deg) rotateY(" + midX + "deg)"
+    })
+
+
+
+    // detect whether mouse is inactive
+    // setup a timer to keep track 
+    function resetTimer() { 
+      window.clearTimeout(mouseTimeout)
+      startTimer()
+    }
+
+    const timeoutCounter = 220
+    let mouseTimeout
   
-  //const cubeTag = document.querySelector('div.cube')
-	
-  const x = event.pageX
-  const y = event.pageY
-  
-  const midX = x - window.innerWidth / 2
-  const midY = y - window.innerHeight / 2
-  
-  cubeTag.style.left = x + 'px'
-  cubeTag.style.top = y + 'px'
-
-  cubeTag.style.transform = "rotateX(" + midY + "deg) rotateY(" + midX + "deg)"
-  
-})
-
-
-document.addEventListener("touchmove", function (event) {
- const cubeTag = document.querySelector('div.cube')
-	
-  const x = event.pageX
-  const y = event.pageY
-  
-  const midX = x - window.innerWidth / 2
-  const midY = y - window.innerHeight / 2
-  
-  cubeTag.style.left = x + 'px'
-  cubeTag.style.top = y + 'px'
-
-  cubeTag.style.transform = "rotateX(" + midY + "deg) rotateY(" + midX + "deg)"
-})
+    function startTimer() { 
+        mouseTimeout = window.setTimeout(mouseIsInactive, timeoutCounter)
+    }
+      
+    function mouseIsInactive() {
+        console.log('im inactive')
+        cubeTag.style.animation = 'spin 2s infinite linear'
+    }
+    
+    function setupTimers () {
+        document.addEventListener("mousemove", resetTimer, false)
+        startTimer()
+    }
+        setupTimers()
+    
