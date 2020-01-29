bd28ec693bb010dc343b845503a6579c1d3bc88e
Jason Booth <hello@studiojasonbooth.com>
2020-01-29 15:14:00 +0000

    mobile bug fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -30,7 +30,16 @@
 
 
     document.addEventListener("touchmove", function (event) {
-      animate()
+      const x = event.pageX
+      const y = event.pageY
+
+      const midX = x - window.innerWidth / 2
+      const midY = y - window.innerHeight / 2
+
+      cubeTag.style.left = x + 'px'
+      cubeTag.style.top = y + 'px'
+
+      cubeTag.style.transform = "rotateX(" + midY + "deg) rotateY(" + midX + "deg)"
     })
 
 
