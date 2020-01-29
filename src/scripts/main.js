e868e0239fb0404903caee1ebb2e415c465460f3
Jason Booth <hello@studiojasonbooth.com>
2020-01-29 14:56:27 +0000

    cleaned up functions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,9 +8,7 @@
       cubeTag.style.animation = ''
     })
 
-
-    document.addEventListener('mousemove', function (event) {
-
+    function animate() {
       const x = event.pageX
       const y = event.pageY
 
@@ -21,23 +19,17 @@
       cubeTag.style.top = y + 'px'
 
       cubeTag.style.transform = "rotateX(" + midY + "deg) rotateY(" + midX + "deg)"
+    }
+
+
+    document.addEventListener('mousemove', function (event) {
+      animate()
     })
       
 
 
     document.addEventListener("touchmove", function (event) {
-      const cubeTag = document.querySelector('div.cube')
-      
-      const x = event.pageX
-      const y = event.pageY
-      
-      const midX = x - window.innerWidth / 2
-      const midY = y - window.innerHeight / 2
-      
-      cubeTag.style.left = x + 'px'
-      cubeTag.style.top = y + 'px'
-
-      cubeTag.style.transform = "rotateX(" + midY + "deg) rotateY(" + midX + "deg)"
+      animate()
     })
 
 
@@ -63,6 +55,7 @@
     
     function setupTimers () {
         document.addEventListener("mousemove", resetTimer, false)
+        document.addEventListener("touchmove", resetTimer, false)
         startTimer()
     }
         setupTimers()
